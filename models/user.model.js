7c3076e36abe3a3bb293f18cb4a67347b460fdc7
tarun <tarun@antino.io>
2021-12-27 16:21:23 +0530

    test jwt

--- models/user.model.js
+++ models/user.model.js
@@ -75,7 +75,7 @@ userSchema.methods.generateToken = async function () {
   const token = jwt.sign({
     _id: user._id.toString(),
     role: user.role
-  }, JWT_key, { expiresIn: '1s' });
+  }, JWT_key, { expiresIn: '1m' });
 
   user.token = token
 
